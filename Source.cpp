d73186090fcec0ab94a35991b42f7aa357456321
FrankiALVarado <42852343+FrankiALVarado@users.noreply.github.com>
2019-02-25 23:57:15 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int sum_seq(int & start, int & end, int & step);
+
+int main() {
+	int start, end, step;
+	while (cin) {
+		cout << "ingrese inicio, fin y step, ctrl z + enter para salir" << endl;
+		cin >> start;
+		cin >> end;
+		cin >> step;
+
+		cout << "la suma de la secuencia es: " << sum_seq(start, end, step) << endl;
+	}
+
+	return 0;
+}
+
+int sum_seq(int & start, int & end, int & step)
+{
+	int rep = (end - start) / step;
+	return start * (rep + 1) + step * (rep *rep + rep) / 2;
+}
